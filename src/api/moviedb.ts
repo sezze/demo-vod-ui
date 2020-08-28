c90819941d935970835adfe797336e0daa9aabef
Sezze <9763776+sezze@users.noreply.github.com>
2020-08-28 12:05:10 +0300

    Removed reduntant configuration fetch

--- src/api/moviedb.ts
+++ src/api/moviedb.ts
@@ -1,9 +1,7 @@
 import { MovieDb } from 'moviedb-promise';
 
-if (!process.env.REACT_APP_API_KEY) throw new Error("No API key provided")
+if (!process.env.REACT_APP_API_KEY) throw new Error('No API key provided');
 
-const moviedb = new MovieDb(process.env.REACT_APP_API_KEY)
-
-export const configuration = moviedb.configuration();
+const moviedb = new MovieDb(process.env.REACT_APP_API_KEY);
 
 export default moviedb;
